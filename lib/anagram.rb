1d40d1d0ce016466e7259ed56d77b95f56787b55
Lrd134 <larryc3200@gmail.com>
2021-03-11 13:17:04 -0500

    commit tree fac33177a21c6e33ccc8a2339d71224ab50cd42e

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(words)
+        @word = words
+    end   
+    def match(array)
+        
+        array.select do | arr |
+            
+            arr.split("").sort == @word.split("").sort
+            
+
+        end
+
+        #array.grep(/[#{@word}]/)
+    end
+end
